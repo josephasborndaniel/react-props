a33c34ac781a9670e10f7e518f652d414cbea2cb
DHINESH-KARTHIK <dheena27072006@gmail.com>
2025-01-21 19:18:09 +0530

    finished of conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
 import Card from "./components/card";
+const  items = [
+  {
+      name: "Margherita Pizza",
+      img: "https://images.unsplash.com/photo-1667207394004-acb6aaf4790e?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      desc: "Classic cheese and tomato pizza with a crispy thin crust.",
+      price: 250,
+      type: "Veg",
+      rating: 3.4
+  },
+  {
+      name: "Butter Chicken",
+      img: "https://images.unsplash.com/photo-1603894584373-5ac82b2ae398?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      desc: "Creamy and rich butter chicken cooked with aromatic spices.",
+      price: 350,
+      type: "Non-Veg",
+      rating: 3.2
+  },
+  {
+      name: "Paneer Tikka",
+      img: "https://images.unsplash.com/photo-1666001120694-3ebe8fd207be?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      desc: "Grilled paneer marinated with spices and yogurt.",
+      price: 200,
+      type: "Veg",
+      rating: 4.6
+  },
+  {
+      name: "Prawn Curry",
+      img: "https://plus.unsplash.com/premium_photo-1669150849060-1d6a6dabad14?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      desc: "Delicious prawn curry in a flavorful coconut-based gravy.",
+      price: 400,
+      type: "Non-Veg",
+      rating: 4.7
+  }
+];
+
+console.log(items);
+
 function App()
 {
   return(
     <>
-    <Card desc='this is god cat'></Card>
-    <h1 className="text-center text-2xl mt-24 font-black">Lorem ipsum dolor sit.</h1>
+    
+      <div className=" m-5 flex justify-center flex-wrap">
+        {
+          items.map(item=> <Card img = {item.img}  title ={item.name} desc = {item.desc} type = {item.type}  ratings = {item.rating} /> )
+
+        }
+      </div>
+
     
     </>
   )
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,12 +1,22 @@
-function Card(props)
+function Card({desc,img,title,type,ratings})
 {
     return(
         <>
-        <div className="border w-[260px] p-2 rounded-xl m-5">
-            <img src="https://images.unsplash.com/photo-1472491235688-bdc81a63246e?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-         <div className="mt-2 text-md text-neutral-600"><p>{props.desc}</p>
-
-         </div>
+        <div className="border w-[260px] p-2 rounded-xl m-5 ">
+            <div className="relative">
+            <img src= {img} alt="" className="w-full h-40 object-cover rounded-lg "/>
+            {
+                ratings >= 3.5 ? (<div className="absolute bottom-0 m-2 px-2 py-[2px] rounded-full bg-green-100 text-green-700 text-sm ">Best Seller</div>): null
+            }
+            
+            </div>
+            <div className="flex justify-between items-center">
+               <p> {title}</p>
+               <span className= { type =='Veg' ? 'h-2 w-3 rounded-full bg-green-800': 'h-2 w-3 rounded-full bg-red-800'}></span>
+                </div>
+         <div className="mt-2 text-sm text-neutral-600">
+            <p>{desc}</p>
+           </div>
         </div>
         </>
     )
