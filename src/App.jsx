90b13e5a7ad4d663b3ec3e8d43d1183c447815af
JOSEPH ASBORN DANIEL J <154415680+josephasborndaniel@users.noreply.github.com>
2025-01-21 19:21:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const  items = [
   {
       name: "Butter Chicken",
       img: "https://images.unsplash.com/photo-1603894584373-5ac82b2ae398?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      desc: "Creamy and rich butter chicken cooked with aromatic spices.",
+      desc: "Creammy and rich butter chicken cooked with aromatic spices.",
       price: 350,
       type: "Non-Veg",
       rating: 3.2
